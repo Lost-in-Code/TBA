46e8e8a18b7264e28a07676431c7badfea02f13a
Gjermund Ask <gjerma14@student.uia.no>
2015-01-23 20:33:42 +0100

    Ajax work

--- static/js/client/client.js
+++ static/js/client/client.js
@@ -13,7 +13,26 @@ $(document).ready(function() {
     });
     $('#joinGame').click(function() {
         if ($('#chooseClass').hasClass("classSet")) {
-            // Set HTML code to user logged in
+            var userNick = $('.userNick').val();
+            var gameID   = $('.userID').val();
+            var userClass   = $('#chooseClass').text();
+            $.ajax({
+                url: '/game/join',
+                data: { room: gameID, nick: userNick, role: userClass},
+                type: 'GET',
+                cache: false,
+                success: function(response) {
+                    console.log(response);
+                    // Set HTML to logged in user
+                    $('body').load("{{ url_for('templates', filename='client/clientcountdown.html') }}")
+                },
+                error: function(error) {
+                    console.log(error);
+                }
+            });
+                
+            // Send info to server, check game ID
+            // Set HTML code to user logged in: Ready button/countdown
         } else {
             $('#chooseClass').html("Select a class! <span class=\"caret\"></span>");
         }
