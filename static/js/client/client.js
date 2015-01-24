31ca22d873cbe8760da0ac1910fbd3c399f3ffa1
Gjermund Ask <gjerma14@student.uia.no>
2015-01-24 14:50:16 +0100

    Start more proper polling, deliver class via number (1:DPS, 2:Tank, 3:Support)

--- static/js/client/client.js
+++ static/js/client/client.js
@@ -4,29 +4,61 @@ var userClass = "";
 var uid       = 0;
 var userState = 0;
 var counter   = 15;
+var roomState = 0;
+var selClass  = 0;
+
+function pollForState(i) {
+    var r = $.Deferred();
+        setInterval (function() {
+            if (userState < i) {
+                $.ajax({
+                    url: '/game/player_status',
+                    data: {uid: uid},
+                    dataType: 'json',
+                    cache: false,
+                    success: function(response) {
+                        userState = response;
+                        console.log("state: " + response);
+                    },
+                    error: function(response) {
+                        alert("Failed to get player state!");
+                        console.log("State: " + response);
+                        return false;
+                    },
+                    complete: pollForState
+                });
+            } else {
+                r.resolve();
+                return r;
+            }
+            
+        }, 1000);
+}
 
 $(document).ready(function() {
     $('.countdown').toggle();
     $('#role_dps').click(function() {
         $('#chooseClass').addClass("classSet");
         $('#chooseClass').html("DPS<span class=\"caret\"></span>");
+        selClass = 1;
     });
     $('#role_tank').click(function() {
         $('#chooseClass').addClass("classSet");
         $('#chooseClass').html("Tank<span class=\"caret\"></span>");
+        selClass = 2;
     });
     $('#role_support').click(function() {
         $('#chooseClass').addClass("classSet");
         $('#chooseClass').html("Support<span class=\"caret\"></span>");
+        selClass = 3;
     });
     $('#joinGame').click(function() {
         if ($('#chooseClass').hasClass("classSet")) {
             userNick  = $('.userNick').val();
             gameID    = $('.userID').val();
-            userClass = $('#chooseClass').text();
             $.ajax({
                 url: '/game/join',
-                data: { room: gameID, nick: userNick, role: userClass},
+                data: { room: gameID, nick: userNick, role: selClass},
                 dataType: 'json',
                 cache: false,
                 success: function(response) {
@@ -34,7 +66,7 @@ $(document).ready(function() {
                         $('.countdown').toggle();
                         $('.joinPage').toggle();
                         uid = response.uid;
-                        console.log(response.uid)
+                        // State is 1
                     } else {
                         alert("Error on join, same nick different class?");
                         console.log(response.uid);
@@ -58,6 +90,7 @@ $(document).ready(function() {
                 cache: false,
                 success: function(response) {
                     console.log(response);
+                    // State is 2
                 },
                 error: function(response) {
                     alert("Couldn't set player as ready");
@@ -65,35 +98,41 @@ $(document).ready(function() {
                 }
             });
             // TODO: Make backend check how many players are ready before this
-            countdownPoll();
+            // Run this until state is 3
+            
+            pollForState(3).done(function() {
+                $('body').html('');
+            });
         } else {
             // Make toggleable?
         }
     });
-});
+}); 
 
-function countdownPoll() {
+
+
+
+/*function gameReadyPoll() {
     setTimeout(function() {
-        if (userState != 3) {
+        if (roomState < 2) {
             $.ajax({
-                url: '/game/player_status',
-                data: {uid: uid},
+                url: '/game/room_status',
+                data: {room_id: gameID},
                 dataType: 'json',
                 cache: false,
                 success: function(response) {
-                    userState = response;  
-                    console.log(response);
-                    $('#theCountdown').text("Game starts in: " + counter);
-                    counter--;
-                },
-                complete: countdownPoll
-                
+                    roomState = response;
+                    console.log("Room state: " + response);
+                    startGame();
+                }
             });
         }
-    }, 1000);
-
-}
-
+    }, 500);
+}*/
 function startGame() {
-    
+    console.log("'Started' game");
 }
+
+
+
+
