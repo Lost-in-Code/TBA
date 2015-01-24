91da0492c5ad4060b2ea88edac912a8abf3b2881
Atle H. Havsø <atle@havso.net>
2015-01-24 14:58:03 +0100

    Renamed a function, check if room exist when trying to join.

--- backend/database.py
+++ backend/database.py
@@ -65,6 +65,8 @@ def db_insert_player(room_id, nick, role):
     db_setup_logging()
     conn = db_get_conn()
     c = conn.cursor()
+    c.execute('''SELECT * FROM games WHERE room_id = ?''', [room_id])
+    if c.fetchone() is None: return (False, 0)
     c.execute('''SELECT * FROM players WHERE room_id = ? AND nick = ?''', [room_id, nick])
     row = c.fetchone()
     if row is None:
